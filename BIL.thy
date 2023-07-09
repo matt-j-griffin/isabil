865964e39d57731ccec0a4595720876e889f2db3
Griffin, Matt J (PG/R - Computer Science) <matt.griffin@surrey.ac.uk>
2023-07-09 10:53:18 +0100

    Fix IsaBIL entrypoint

--- BIL.thy
+++
@@ -1,8 +0,0 @@
-theory BIL
-  imports Program_Model
-begin              
-
-
-(* TODO hide consts etc... here *)
-
-end
---
+++ IsaBIL.thy
@@ -0,0 +1,9 @@
+theory IsaBIL
+  imports "Operational_Semantics"
+          "BIL_Parse"
+begin
+
+
+(* TODO hide consts etc... here *)
+
+end
